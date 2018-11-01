e938d1ac908ea7c218d003f209c13addda98652a
Timithy Snedden <s189066@students.aie.edu>
2018-11-01 09:06:28 -0700

    V7

--- LinkedLists/Main.cpp
+++ LinkedLists/Main.cpp
@@ -1,4 +1,11 @@
 #include"tForwardList.h"
 int main() {
-
+	tForwardList<int> link;
+	link.push_front(4);
+	link.pop_front();
+	link.push_front(9);
+	link.pop_front();
+	link.push_front(57);
+	std::cout << link.front()<< std::endl;
+	return 0;
 }
--- LinkedLists/tForwardList.h
+++ LinkedLists/tForwardList.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 template<typename T>
 class tForwardList
 {
@@ -18,8 +19,6 @@ public:
 	void pop_front();               // removes element from front
 
 	T& front();                     // returns the element at the head
-	const T& front();               // returns the element at the head (const)
-
 	void remove(const T& val);      // removes all elements equal to the given value
 };
 
@@ -36,20 +35,44 @@ tForwardList<T>::~tForwardList()
 template<typename T>
 void tForwardList<T>::push_front(const T & val)
 {
+	Node *temp = new Node;
+	temp->next = head;
+	temp->data = val;
+	head = temp;
+
 }
 
 template<typename T>
 void tForwardList<T>::pop_front()
 {
+	Node *temp = new Node;
+	if (head == NULL) {
+		return;
+	}
+	temp = head->next;
+	delete head;
+	head = temp;
 }
 
 template<typename T>
 T & tForwardList<T>::front()
 {
-	// TODO: insert return statement here
+	return head->data;
 }
 
 template<typename T>
 void tForwardList<T>::remove(const T & val)
 {
+	Node *temp = new Node;
+	if (head == NULL) {
+		return;
+	}
+	else if (head->data == val) {
+		temp = head->next;
+		delete head;
+		head = temp;
+	}
+	else {
+		head = head->next;
+	}
 }
